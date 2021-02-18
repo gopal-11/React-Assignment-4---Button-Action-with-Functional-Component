4f756b5d532ec5c4e56324b905d1f52825953510
gopal-11 <gopalbhilotiya98@gmail.com>
2021-02-18 17:39:33 +0530

    final

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
+
 function App() {
+  const [flag,setFlag]=useState(false);
+const handleClick=()=>{
+  setFlag(true);
+}
   return (
     <div id="main">
-      // Do not alter the main div
+      <button onClick={handleClick}>click me</button>
+      {flag?(
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>
+        ) : null}
     </div>
   );
 }
